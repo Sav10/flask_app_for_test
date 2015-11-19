7bee0e1172b1c7e2efede01cf8d0f52da5f8a5e8
Sav10 <sav.derivet@gmail.com>
2015-11-19 15:07:21 +0100

    f3

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 MyApp = Flask(__name__)
+
+var_01 = "this is my first var"
 
 @MyApp.route("/")
 def hello():
-	return render_template('index.html')
+	return render_template('index.html', title = 'Home')
 
-@MyApp.route("/about2")
+@MyApp.route("/about")
 def about():
-	return render_template('about.html')
+	return render_template('about.html', title = 'About', my_first_var = var_01)
 
 
 @MyApp.route("/contact")
 def contact():
-	return render_template('contact.html')
+	return render_template('contact.html', title = 'Contact')
 
 if __name__ == "__main__":
 	MyApp.run(debug=True)
