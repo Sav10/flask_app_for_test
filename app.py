6e402e20053debd733aa4cc991facb490f781b47
Sav10 <sav.derivet@gmail.com>
2015-11-19 12:29:18 +0100

    f1

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ MyApp = Flask(__name__)
 def hello():
 	return render_template('index.html')
 
+@MyApp.route("/about")
+def about():
+	return render_template('about.html')
+
+
+@MyApp.route("/contact")
+def contact():
+	return render_template('contact.html')
+
 if __name__ == "__main__":
-	MyApp.run()
+	MyApp.run(debug=True)
